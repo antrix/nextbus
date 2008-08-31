c6ea153e3f8f8a1edb43cacc18423fd3f09d5e18
Deepak Sarda <deepak@antrix.net>
2008-08-31 20:02:43 +0800

    Everything works on localhost

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import wsgiref.handlers
 import logging
 import os
+import re
 
 from google.appengine.ext import webapp
 from google.appengine.api import memcache
 from google.appengine.api import urlfetch
-from google.appengine.runtime import DeadlineExceededError
+try:
+    from google.appengine.runtime import DeadlineExceededError
+except:
+    from google.appengine.runtime.apiproxy_errors import DeadlineExceededError
 
 from BeautifulSoup import BeautifulSoup
 
-SBS_SITE = 'http://www.sbstransit.com.sg/mobileiris/'
+SBS_SITE = 'http://www.sbstransit.com.sg/mobileiris'
 
 def isvaliddomain():
     valid_list = ['nextbus.appspot.com', 
@@ -53,34 +57,37 @@ class EndPoint(webapp.RequestHandler):
         try:
             # Fetch the bus services at this stop
             try:
-                services, stop_description = self.get_stop_details(stop)
+                services = self.get_stop_details(stop)
             except:
                 self.error(500)
                 self.response.out.write('Error processing request. Please retry.')
                 logging.error("Error getting stop details", exc_info=True)
                 return
 
-            self.response.out.write('<html><head><title>Services at Stop %s' \
-                    '</title></head><body><p>Stop %s <br/> %s</p>' \
-                    '<table>' % (stop, stop, stop_description))
+            self.response.out.write('<html><head><title>Services at stop %s' \
+                    '</title></head><style type="text/css">body, td, th {font-size: smaller;}' \
+                    '</style><body><p>Services at stop %s</p>' \
+                    '<font><table border="0" cellpadding="2"><tr align="left"><th></th><th>Next</th>' \
+                    '<th>Subsequent</th></tr>' % (stop, stop))
             for service in services:
                 try:
                     next, subsequent = self.get_timings(stop, service)
                 except:
-                    self.error(500)
-                    self.response.out.write('Error processing request. Please retry.')
+                    next, subsequent = ('retry', 'retry')
                     logging.error("Error getting service details", exc_info=True)
-                    return
 
-                self.response.out.write('<tr><td>%s</td><td>%s</td>' \
-                        '<td>%s</td></tr>' % (service, next, subsequent))
+                self.response.out.write('<tr><td><a href="%s/index_mobresult.aspx?' \
+                        'stop=%s&svc=%s">%s</a></td><td>%s</td><td>%s</td></tr>' \
+                        % (SBS_SITE, stop, service, service, next, subsequent))
 
-            self.response.out.write('</table></body></html>')
+            self.response.out.write('</table><br/><a href="http://%s/stop/?number=%s">' \
+                    'Refresh</a></font></body></html>' % (os.environ['HTTP_HOST'].lower(),
+                                                            stop))
             return
         except DeadlineExceededError:
             redirect_url = '/stop/?number=%s&random=%s' % (stop, random.randint(1,100))
             self.redirect(redirect_url)
-            logging.debug('Request preempted while processing %s for stop %s.' \
+            logging.info('Request preempted while processing %s for stop %s.' \
                     'Redirecting to %s' % service, stop, redirect_url)
 
 
@@ -88,44 +95,58 @@ class EndPoint(webapp.RequestHandler):
 
         details = memcache.get(stop)
         if details is not None:
-            logging.info('Cache hit for stop %s' % stop)
-            return (details[1:], details[0]) # (services, description)
+            logging.debug('Cache hit for stop %s' % stop)
+            return details
 
         result = get_url('%s/index_svclist.aspx?stopcode=%s' % (SBS_SITE, stop))
 
         soup = BeautifulSoup(result)
 
-        stop_description = ' '.join([unicode(x) for x in soup.font.contents])
-
         services = []
-        for link in soup.table('a'):
+        for link in soup('a'):
             services.append(unicode(link.string))
 
-        logging.debug('for stop %s at %s services are %s' % (
-                    stop, stop_description, services))
+        logging.debug('for stop %s services are %s' % (stop, services))
 
         # save to memcache
-        if not memcache.set(stop, (stop_description, services)):
+        if not memcache.set(stop, services):
             logging.error('Failed saving cache for stop %s' % stop)
         else:
             logging.debug('Saved stop %s to cache' % stop)
 
-        return (services, stop_description)
+        return services
 
 
     def get_timings(self, stop, service):
 
-        next, subsequent = memcache.get('%s-%s' % (stop, service))
-        if next is not None and subsequent is not None:
+        cached = memcache.get('%s-%s' % (stop, service))
+        if cached is not None:
             logging.debug('Cache hit for service %s at stop %s' \
                     % (service, stop))
-            return (next, subsequent)
+            return cached
 
         result = get_url('%s/index_mobresult.aspx?stop=%s&svc=%s' \
                         % (SBS_SITE, stop, service))
 
-        x = re.search(r'Next bus:\s+(?P<next>[\w\s]+)<br>.*?Subsequent bus:' \
-                    '\s+(?P<subsequent>[\w\s]+)</font>', html, re.DOTALL)
+        #logging.info(result)
+        # Check if we are redirected
+        if re.search(r'Object moved to', result, re.DOTALL):
+            soup = BeautifulSoup(result)
+            new_url = soup('a')[0]['href']
+            logging.debug('new url: %s' % new_url)
+            token = re.search(r'\(\w+?\)', new_url).group()
+            logging.info('new token: %s' % token)
+            result = get_url('%s/%s/index_mobresult.aspx?stop=%s&svc=%s' \
+                            % (SBS_SITE, token, stop, service))
+            #logging.info(result)
+
+        x = re.search(r'Next bus:\s+(?P<next>[\w\s\(\)]+)<br>.*?Subsequent bus:' \
+                    '\s+(?P<subsequent>[\w\s\(\)]+)<br>', result, re.DOTALL)
+
+        if not x:
+            logging.debug('failed to fetch info for service %s at stop %s' % (
+                                service, stop))
+            return ('retry', 'retry')
 
         next = x.group('next')
         subsequent = x.group('subsequent')
@@ -143,8 +164,15 @@ class EndPoint(webapp.RequestHandler):
 urls = [('/stop\/?', EndPoint)]
 
 def main():
+    if os.environ['HTTP_HOST'].lower() in ('nextbus.appspot.com',):
+        logging.getLogger().setLevel(logging.INFO)
+        debug = False
+    else:
+        logging.getLogger().setLevel(logging.DEBUG)
+        debug = True
+
     application = webapp.WSGIApplication(
-          urls, debug=True)
+          urls, debug=debug)
     wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == "__main__":
