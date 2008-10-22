cb159d5301f1439e885dd93bb369c7ba7c1f7aa4
Deepak Sarda <deepak@antrix.net>
2008-10-23 00:30:44 +0800

    Added rudimentary AJAX support detection to homepage

---
+++ static/index.js
@@ -0,0 +1,35 @@
+function init() {
+    function mark_error() {
+        document.getElementById('error').style.color = 'red';
+    }
+    var n_f = document.inputform.number;
+    document.inputform.onsubmit = function() {
+        var stop = n_f.value;
+        if (stop.length != 5) {
+            mark_error();
+            return false;
+        }
+        var date = new Date();
+        date.setTime(date.getTime()+(60*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+        document.cookie = "Stop="+stop+expires+"; path=/";
+        return true;
+    }
+    var c = document.cookie.split(';');
+    for (var i=0; i < c.length; i++) {
+        if (c[i].indexOf('Stop') != -1) {
+            n_f.value = c[i].split('=')[1];
+        }
+    }
+    if (document.location.search.indexOf('error') > 0) {
+        mark_error();
+    }
+    n_f.focus();
+    n_f.select();
+    /* Check if browser support XMLHttpRequest */
+    if (window.XMLHttpRequest) {
+        n_f.innerHTML += '<input type="hidden" name="xhr" value="1" />';
+        document.getElementById('debug').innerHTML = 'debug: browser supports ajax';
+    } 
+}
+init();
